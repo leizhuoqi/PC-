b33423a41ea1b08fc070237fc93ff973a3a4cb4e
leizhuoqi <415339660@qq.com>
2019-01-04 20:09:16 +0800

    改进滚轮事件

--- js/index.js
+++ js/index.js
@@ -7,11 +7,12 @@ window.addEventListener('DOMContentLoaded',function(){
  var contentList = document.querySelector('.contentList');
  var contentNode = document.querySelector('.content');
  var contentHeight = contentNode.offsetHeight;
+ var arrowHalfWidth = arrowNode.offsetWidth/2;
  var nowIndex = 0;
 
  header();
     function header(){
-        arrowNode.style.left = headerLiNodes[0].getBoundingClientRect().left + liHalfWidth - arrowNode.offsetWidth/2 + 'px';
+        arrowNode.style.left = headerLiNodes[0].getBoundingClientRect().left + liHalfWidth - arrowHalfWidth + 'px';
         headerDownNodes[0].style.width = '100%';
 
         for (var i = 0; i < headerLiNodes.length; i++) {
@@ -30,53 +31,63 @@ window.addEventListener('DOMContentLoaded',function(){
         }
         headerDownNodes[nowIndex].style.width = '100%';
         arrowNode.style.left = headerLiNodes[nowIndex].getBoundingClientRect().left + headerLiNodes[nowIndex].offsetWidth/2
-            - arrowNode.offsetWidth/2 + 'px';
+            - arrowHalfWidth + 'px';
         contentList.style.top = - nowIndex * contentHeight + 'px';
     }
+    var timer = null;
 
-    content();
+        content();
     function content(){
         document.onmousewheel = wheel;
         document.addEventListener('DOMMouseScroll',wheel);
 
         function wheel (event){
-
             event = event || window.event;
-            var flag ='';
-            //ie chrome
-            if(event.wheelDelta){
-                if(event.wheelDelta > 0){
-                    flag = 'up';
-                }else{
-                    flag = 'down';
-                }
-            }else if(event.detail){
-                if(event.detail > 0){
-                    flag = 'down';
-                }else{
-                    flag = 'up';
-                }
-            }
-
-            switch (flag){
-                case 'up':
-                if(nowIndex > 0){
-                    nowIndex--;
-                    move(nowIndex);
-                }
-                break;
-                case'down':
-                    if(nowIndex < 4){
-                        nowIndex++;
-                        move(nowIndex);
+            clearTimeout(timer)
+            timer = setTimeout(function(){
+                var flag ='';
+                //ie chrome
+                if(event.wheelDelta){
+                    if(event.wheelDelta > 0){
+                        flag = 'up';
+                    }else{
+                        flag = 'down';
                     }
-                    break;
-            }
+                }else if(event.detail){
+                    if(event.detail > 0){
+                        flag = 'down';
+                    }else{
+                        flag = 'up';
+                    }
+                }
+
+                switch (flag){
+                    case 'up':
+                        if(nowIndex > 0){
+                            nowIndex--;
+                            move(nowIndex);
+                        }
+                        break;
+                    case'down':
+                        if(nowIndex < 4){
+                            nowIndex++;
+                            move(nowIndex);
+                        }
+                        break;
+                }
+            },150)
 
             event.preventDefault && event.preventDefault();
             return false;
         }
 
+    }
+
+
+    window.onresize = function () {
+        arrowNode.style.left = headerLiNodes[nowIndex].offsetLeft + headerDownNodes[nowIndex].offsetWidth/2
+            - arrowHalfWidth + 'px';
+        contentList.style.top = -nowIndex * contentHeight +'px';
     }
 
 })
