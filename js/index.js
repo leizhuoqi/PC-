c1c2972944ef84d47a2a46a9dae292f5ea849a46
leizhuoqi <415339660@qq.com>
2019-01-06 21:49:16 +0800

    基本全部完成（除canvas动画效果）

--- js/index.js
+++ js/index.js
@@ -9,7 +9,9 @@ window.addEventListener('DOMContentLoaded',function(){
  var contentHeight = contentNode.offsetHeight;
  var arrowHalfWidth = arrowNode.offsetWidth/2;
  var nowIndex = 0;
+ var lastIndex= 0;
  var timer = null;
+    var navLiNodes = document.querySelectorAll('.content .navBar li');
 
 
     header();
@@ -30,11 +32,15 @@ window.addEventListener('DOMContentLoaded',function(){
     function move(nowIndex){
         for (var j = 0; j < headerDownNodes.length; j++) {
             headerDownNodes[j].style.width = '';
+
         }
         headerDownNodes[nowIndex].style.width = '100%';
         arrowNode.style.left = headerLiNodes[nowIndex].getBoundingClientRect().left + headerLiNodes[nowIndex].offsetWidth/2
             - arrowHalfWidth + 'px';
         contentList.style.top = - nowIndex * contentHeight + 'px';
+        navLiNodes[lastIndex].className = '';
+        navLiNodes[nowIndex].className = 'active';
+        lastIndex = nowIndex;
     }
 
 
@@ -83,6 +89,107 @@ window.addEventListener('DOMContentLoaded',function(){
             return false;
         }
 
+        for (var i = 0; i <navLiNodes .length; i++) {
+            navLiNodes[i].index = i;
+            navLiNodes[i].onclick = function(){
+                nowIndex = this.index;
+                move(nowIndex)
+            }
+        }
+        // navBar();
+        // function navBar(){
+        //
+        //     var lastIndex = 0;
+        //     for (var i = 0; i <navLiNodes .length; i++) {
+        //         navLiNodes[i].index = i;
+        //         navLiNodes[i].onclick = function(){
+        //             nowIndex =this.index;
+        //             for (var j = 0; j <navLiNodes .length; j++) {
+        //                 navLiNodes[j].className = ''
+        //             }
+        //             move(nowIndex)
+        //             navLiNodes[nowIndex].className = 'active';
+        //         }
+        //
+        //     }
+        //
+        //     lastIndex = nowIndex;
+        // }
+
+
+        banner();
+        function banner(){
+            var banLiNodes = document.querySelectorAll('.banner li')
+            var homeNode = document.querySelector('.home');
+            var pointLiNodes = document.querySelectorAll('.circlePoint li');
+            var nowIndex = 0;
+            var lastIndex = 0;
+            var lastTime = 0;
+            var timer = null;
+            for (var i = 0; i <pointLiNodes.length; i++) {
+                pointLiNodes[i].index = i;
+                pointLiNodes[i].onclick = function(){
+                    var nowTime = new Date();
+                    if(nowTime - lastTime <1000) return;
+                    lastTime = nowTime;
+                    nowIndex = this.index;
+                    if(nowIndex == lastIndex) return;
+                    if(nowIndex < lastIndex){
+                        banLiNodes[nowIndex].className = 'common-title left-show';
+                        banLiNodes[lastIndex].className = 'common-title right-hide'
+
+                    }else{
+                        banLiNodes[nowIndex].className = 'common-title right-show';
+                        banLiNodes[lastIndex].className = 'common-title left-hide'
+                    }
+                    pointLiNodes[lastIndex].className = '';
+                    pointLiNodes[nowIndex].className = 'active';
+                    lastIndex = nowIndex ;
+                }
+            }
+
+            homeNode.onmouseenter = function(){
+                clearInterval(timer)
+            };
+            homeNode.onmouseleave = bannerAuto;
+
+            bannerAuto();
+            function bannerAuto(){
+                timer = setInterval(function(){
+                    nowIndex++;
+                    if(nowIndex >= 4) nowIndex =0 ;
+                    banLiNodes[nowIndex].className = 'common-title right-show';
+                    banLiNodes[lastIndex].className = 'common-title left-hide'
+
+                    pointLiNodes[lastIndex].className = '';
+                    pointLiNodes[nowIndex].className = 'active';
+
+                    lastIndex = nowIndex;
+                },1500)
+            }
+        }
+
+        teamAnimation();
+        function teamAnimation(){
+            var teamList = document.querySelector('.fifthScreen .team-list');
+            var teamLiNodes = document.querySelectorAll('.fifthScreen .team-list li');
+            for (var i = 0; i <teamLiNodes.length; i++) {
+
+                teamLiNodes[i].onmouseenter = function(){
+                    for (var j = 0; j <teamLiNodes .length; j++) {
+                        console.log(111)
+                        teamLiNodes[j].style.opacity = 0.5;
+                    }
+                    this.style.opacity = 1;
+                }
+            }
+            teamList.onmouseleave = function(){
+                for (var i = 0; i <teamLiNodes.length; i++) {
+                    teamLiNodes[i].style.opacity = '1'
+                }
+            }
+        }
+
     }
 
 
@@ -95,54 +202,5 @@ window.addEventListener('DOMContentLoaded',function(){
     }
 
 
-
-
-    banner();
-    function banner(){
-        var banLiNodes = document.querySelectorAll('.banner li')
-        var circlePoint = document.querySelector('.circlePoint');
-        var pointLiNodes = document.querySelectorAll('.circlePoint li');
-        var nowIndex = 0;
-        var lastIndex = 0;
-        var lastTime = 0;
-        for (var i = 0; i <pointLiNodes.length; i++) {
-            pointLiNodes[i].index = i;
-            pointLiNodes[i].onclick = function(){
-                var nowTime = new Date();
-                if(nowTime - lastTime <2000) return;
-                lastTime = nowTime;
-                 nowIndex = this.index;
-                if(nowIndex == lastIndex) return;
-                if(nowIndex < lastIndex){
-                    banLiNodes[nowIndex].className = 'common-title left-show';
-                    banLiNodes[lastIndex].className = 'common-title right-hide'
-
-                }else{
-                    banLiNodes[nowIndex].className = 'common-title right-show';
-                    banLiNodes[lastIndex].className = 'common-title left-hide'
-
-
-                }
-                pointLiNodes[lastIndex].className = '';
-                pointLiNodes[nowIndex].className = 'active';
-                console.log(111)
-                lastIndex = nowIndex ;
-            }
-        }
-        bannerAuto();
-        function bannerAuto(){
-            timer = setInterval(function(){
-                nowIndex++;
-                if(nowIndex >= 4) nowIndex =0 ;
-                banLiNodes[nowIndex].className = 'common-title right-show';
-                banLiNodes[lastIndex].className = 'common-title left-hide'
-
-                pointLiNodes[lastIndex].className = '';
-                pointLiNodes[nowIndex].className = 'active';
-
-                lastIndex = nowIndex;
-            },2200)
-        }
-    }
 
 })
